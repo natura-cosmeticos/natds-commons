52231a3352c1b4ee824325d67ca9c8f463fd6be3
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-08-24 19:47:13 -0300

    test(natds-themes): change syntax to CommonJS
    DSY-1413
    
    Co-Authored-By: Ariel Flor <arielwb@gmail.com>

--- packages/natds-themes/jest.setup.js
+++ packages/natds-themes/jest.setup.js
@@ -1,4 +1,8 @@
-import { toMatchFile } from 'jest-file-snapshot';
+/**
+ * @todo create adr for `jest-file-snapshot` use decision
+ */
+const { toMatchFile } = require('jest-file-snapshot');
+// import { toMatchFile } from 'jest-file-snapshot';
 
 expect.extend({ toMatchFile });
 
@@ -6,8 +10,6 @@ const currentDate = new Date('2020-05-02T11:01:58.135Z');
 
 global.Date = class extends Date {
   constructor() {
-    super();
-
     return currentDate;
   }
 };
