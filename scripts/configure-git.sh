afd73d21d7cb04db1a3e08d9e9a6a7b86fefba07
Ariel Flor <arielwb@gmail.com>
2020-11-13 16:46:50 -0300

    chore: update pipeline user

--- scripts/configure-git.sh
+++ scripts/configure-git.sh
@@ -7,5 +7,5 @@ git remote add origin "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}"
 git fetch --quiet
 git checkout "${TRAVIS_BRANCH}"
 
-git config --global user.email "systemteamopensource@natura.net"
-git config --global user.name "${GITHUB_API_USER} in behalf of System Team Open Source"
+git config --global user.email $GITHUB_USER_EMAIL
+git config --global user.name "${GITHUB_USER_NAME} in behalf of Design System Team"
