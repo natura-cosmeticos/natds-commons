b3fdf656a7a68039b36a69188bbdf49fed365591
Ariel Flor <arielwb@gmail.com>
2020-11-17 16:46:27 -0300

    chore(natds-icons): add icons input tests
    affects: @naturacosmeticos/natds-icons
    
    DSY-1668

---
+++ packages/natds-icons/inputs/icons-input.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable mocha/no-setup-in-describe */
+import fs from 'fs';
+import path from 'path';
+
+const getAllIcons = () => {
+  const svgFolder = path.resolve(__dirname, '../svg');
+  const icons = fs
+    .readdirSync(svgFolder)
+    .filter((file) => file.includes('.svg'))
+    .map((name) => ({
+      content: fs.readFileSync(path.resolve(svgFolder, name)).toString(),
+      name,
+    }));
+
+  return icons;
+};
+
+describe('icons input', () => {
+  const icons = getAllIcons();
+
+  test.each(icons)('should have the correct size %o', (icon) => {
+    const matchSize = /viewBox="(\d+\s\d+\s\d+\s\d+)"/;
+    const size = icon.content.match(matchSize);
+
+    expect(size[1]).toEqual('0 0 24 24');
+  });
+
+  test.each(icons)('should have the correct naming convention %o', (icon) => {
+    const words = icon.name.split('-');
+
+    const isAllowedStyleName = words[0] === 'filled' || words[0] === 'outlined';
+
+    expect(words.length).toEqual(3);
+    expect(isAllowedStyleName).toBeTruthy();
+  });
+});
