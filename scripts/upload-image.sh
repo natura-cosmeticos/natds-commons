951f14fb1e9b497c68a9d7a278074be0e38b2532
Ariel Flor <arielwb@gmail.com>
2020-11-19 11:14:33 -0300

    chore(natds-icons): change pipeline config to run regression tests and include generated files
    DSY-1668

---
+++ scripts/upload-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+URL="https://api.imgur.com/3/image"
+
+FILE="$(pwd)/packages/natds-icons/regression/__image_snapshots__/__diff_output__/icons-test-js-icon-list-should-match-the-previous-screenshot-1-diff.png"
+
+if test -f "$FILE"; then
+  IMAGE="@/${FILE}"
+
+  IMAGE_DATA=$(curl --silent --request POST --url $URL --header "authorization: Client-ID ${IMGUR_CLIENT_ID}" --header 'content-type: multipart/form-data;' -F "image=${IMAGE}")
+
+  node -e "const imgurResponse = ${IMAGE_DATA}; console.log(imgurResponse && imgurResponse.data && imgurResponse.data.link || imgurResponse)"
+
+fi
