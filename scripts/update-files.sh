b4e717f6dd6d46e95f48b62204116841c8e75e0c
Ariel Flor <arielwb@gmail.com>
2020-11-19 11:14:33 -0300

    chore(natds-icons): change pipeline config to run regression tests and include generated files
    DSY-1668

---
+++ scripts/update-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+git add packages/natds-themes/build/
+
+git add packages/natds-icons/dist/
+git add packages/natds-icons/regression/
+git add packages/natds-icons/src/generated/
+
+git commit --allow-empty -m "chore: update generated files [skip ci]"
+git push
