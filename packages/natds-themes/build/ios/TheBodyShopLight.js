f2293d037867842610884dbb99c17428c81bd00c
Bitrise CI in behalf of Design System Team <designsystem@natura.net>
2020-12-04 14:09:35 +0000

    chore: update generated files [ci skip]

--- packages/natds-themes/build/ios/TheBodyShopLight.js
+++ packages/natds-themes/build/ios/TheBodyShopLight.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Fri, 04 Dec 2020 14:01:43 GMT
+ * Generated on Fri, 04 Dec 2020 14:09:09 GMT
  */
 
 module.exports = {
--- packages/natds-themes/build/web/aesop/dark-esm.js
+++ packages/natds-themes/build/web/aesop/dark-esm.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Fri, 04 Dec 2020 14:01:42 GMT
+ * Generated on Fri, 04 Dec 2020 14:09:08 GMT
  */
 
 module.exports = {
