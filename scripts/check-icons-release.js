33fe7a5350cce812a4d5cea3d5701f5c64ee59bb
Ariel Flor <arielwb@gmail.com>
2020-12-04 18:17:56 -0300

    chore: update regression update workflow, fix script

--- scripts/check-icons-release.js
+++ scripts/check-icons-release.js
@@ -1,11 +1,7 @@
 const { exec } = require('child_process');
 
 exec('npx lerna changed --json --loglevel=silent', (err, stdout) => {
-  if (err) {
-    return;
-  }
-
-  const changed = JSON.parse(stdout).find((item) => item.name === '@naturacosmeticos/natds-icons');
+  const changed = stdout && JSON.parse(stdout).find((item) => item.name === '@naturacosmeticos/natds-icons');
 
   console.log(!!changed);
 });
