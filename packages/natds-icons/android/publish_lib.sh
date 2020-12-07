325fcc3fd8f84dd763a43d8fe710dcc9010b6d48
Ariel Flor <arielwb@gmail.com>
2020-12-07 14:05:02 -0300

    chore: add check on android icons release

--- packages/natds-icons/android/publish_lib.sh
+++ packages/natds-icons/android/publish_lib.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
-export NATDS_ICONS_VERSION=$(packages/natds-cli/bin/getPackageVersion.js packages/natds-icons/package.json)
-mkdir -p "packages/natds-icons/android/icons/src/main/res/drawable"
-cp packages/natds-icons/dist/drawables/* packages/natds-icons/android/icons/src/main/res/drawable
-cd packages/natds-icons/android
-gradle icon:assemble
-cd icons
-gradle publish --stacktrace
+#!/bin/bash
+if [[ $WILL_RELEASE_ICONS = true ]]; then
+  export NATDS_ICONS_VERSION=$(packages/natds-cli/bin/getPackageVersion.js packages/natds-icons/package.json)
+  mkdir -p "packages/natds-icons/android/icons/src/main/res/drawable"
+  cp packages/natds-icons/dist/drawables/* packages/natds-icons/android/icons/src/main/res/drawable
+  cd packages/natds-icons/android
+  gradle icon:assemble
+  cd icons
+  gradle publish --stacktrace
+fi
