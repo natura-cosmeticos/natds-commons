5f88a5c6b2c21e8772f70beac98179d481f4d911
Ariel Flor <arielwb@gmail.com>
2020-12-07 11:15:04 -0300

    feat: add configure git and npm scrips

--- packages/natds-cli/bin/configureGit.sh
+++ packages/natds-cli/bin/configureGit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-git remote rm origin
-git remote add origin "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}"
+git init
+git remote add origin "https://${GITHUB_TOKEN}@github.com/natura-cosmeticos/${BITRISEIO_GIT_REPOSITORY_SLUG}"
 
 git fetch --quiet
-git checkout "${TRAVIS_BRANCH}"
+git checkout $BITRISE_GIT_BRANCH
 
 git config --global user.email $GITHUB_USER_EMAIL
 git config --global user.name "${GITHUB_USER_NAME} in behalf of Design System Team"
