78a607a9a5221e3aa6cb1e69609c3fb7d841927a
Ariel Flor <arielwb@gmail.com>
2020-12-16 13:37:30 -0300

    chore: change pipeline config to deploy PRs to s3
    DSY-000

---
+++ .cicd/build-html.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+[[ -z "${CHANGE_BRANCH}" ]] && BRANCH=$BRANCH_NAME || BRANCH=$CHANGE_BRANCH
+
+FOLDER_NAME=${BRANCH//[\/]/-}
+
+[[ $1 = "master" ]] && OUTPUT_PATH=public || OUTPUT_PATH="public/${FOLDER_NAME}"
+
+echo "Verifying $1"
+
+mkdir -p ${OUTPUT_PATH}
+echo "Created ${FOLDER_NAME} folder at ${OUTPUT_PATH}"
+
+yarn install
+yarn html:build
+
+cp -r build/* ${OUTPUT_PATH}
+
