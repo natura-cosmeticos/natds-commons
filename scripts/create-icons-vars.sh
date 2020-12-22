d6484741ca62eb4677bbbf657c046a4383b15df9
Ariel Flor <arielwb@gmail.com>
2020-12-22 12:12:54 -0300

    chore: fix icons env var creation script, add ios icons to ignore
    co-author: @penelopearaujo @vitorpirestw
    
    DSY-1742

--- scripts/create-icons-vars.sh
+++ scripts/create-icons-vars.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-export NATDS_ICONS_VERSION=$(../../../natds-cli/bin/getPackageVersion.js ../../../natds-icons/package.json)
-echo "Getting version ${NATDS_ICONS_VERSION} info"
+ICONS_VERSION=$(../../../natds-cli/bin/getPackageVersion.js ../../../natds-icons/package.json)
+echo "Natds icons version ${NATDS_ICONS_VERSION}"
 
-export NATDS_ICONS_TAG=@naturacosmeticos/natds-icons@${NATDS_ICONS_VERSION}
-echo "Publishing tag ${NATDS_ICONS_TAG}"
+ICONS_TAG=@naturacosmeticos/natds-icons@${NATDS_ICONS_VERSION}
+echo "Natds icons tag ${NATDS_ICONS_TAG}"
+
+envman add --key NATDS_ICONS_VERSION --value ICONS_VERSION
+envman add --key NATDS_ICONS_TAG --value ICONS_TAG
