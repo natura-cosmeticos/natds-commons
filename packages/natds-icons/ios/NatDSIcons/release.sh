f9e9966454c48eb02632f6b4be0d2219e6a82ec4
Ariel Flor <arielwb@gmail.com>
2020-12-22 11:00:39 -0300

    chore: add files from dist to ios project
    co-author: @penelopearaujo, @vitorpirestw
    
    DSY-1742

--- packages/natds-icons/ios/NatDSIcons/release.sh
+++ packages/natds-icons/ios/NatDSIcons/release.sh
@@ -2,8 +2,9 @@
 
 if [[ $WILL_RELEASE_ICONS = true ]]; then
   echo "Publishing version ${NATDS_ICONS_VERSION} at tag ${NATDS_ICONS_TAG}"
-  pod spec lint
-  # pod trunk push NatDSIcons.podspec
+  cp ../../dist/fonts/natds-icons.ttf ./Resources/natds-icons-ios.ttf
+  cp ../../dist/natds-icons.swift ./NatDSIcons/Icon.swift
+  pod trunk push NatDSIcons.podspec
 fi
 
 echo "Not publishing icons, skipping..."
