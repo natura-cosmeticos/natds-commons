71d1125936c15a5a9dbca77225b57039bf312269
Ariel Flor <arielwb@gmail.com>
2020-12-22 11:39:23 -0300

    chore: update ios files before tag creation
    co-author: @penelopearaujo @vitorpirestw
    
    DSY-1742

--- packages/natds-icons/ios/NatDSIcons/release.sh
+++ packages/natds-icons/ios/NatDSIcons/release.sh
@@ -2,8 +2,6 @@
 
 if [[ $WILL_RELEASE_ICONS = true ]]; then
   echo "Publishing version ${NATDS_ICONS_VERSION} at tag ${NATDS_ICONS_TAG}"
-  cp ../../dist/fonts/natds-icons.ttf ./Resources/natds-icons-ios.ttf
-  cp ../../dist/natds-icons.swift ./NatDSIcons/Icon.swift
   pod trunk push NatDSIcons.podspec
 fi
 
