2bea404ebe4b8489b5fb60ef565d98a78881a7d9
Penélope Araújo <pmpa@cin.ufpe.br>
2021-01-13 16:34:36 -0300

    fix: [DSY-1750] update create icons vars

--- scripts/create-icons-vars.sh
+++ scripts/create-icons-vars.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-ICONS_VERSION=$(../../../natds-cli/bin/getPackageVersion.js ../../../natds-icons/package.json)
-echo "Natds icons version ${NATDS_ICONS_VERSION}"
+ICONS_VERSION=$(packages/natds-cli/bin/getPackageVersion.js ./packages/natds-icons/package.json)
+echo "Natds icons version ${ICONS_VERSION}"
 
-ICONS_TAG=@naturacosmeticos/natds-icons@${NATDS_ICONS_VERSION}
-echo "Natds icons tag ${NATDS_ICONS_TAG}"
+ICONS_TAG=@naturacosmeticos/natds-icons@${ICONS_VERSION}
+echo "Natds icons tag ${ICONS_TAG}"
 
 envman add --key NATDS_ICONS_VERSION --value ICONS_VERSION
 envman add --key NATDS_ICONS_TAG --value ICONS_TAG
