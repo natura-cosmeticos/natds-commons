7e3d4973bd18c73201dc7aa6f19be7e29d99ff77
Design system Natura in behalf of Design System Team <designsystem@natura.net>
2022-01-20 20:02:09 +0000

    chore: update generated files [ci skip]

--- packages/natds-themes/build/react-native/index.js
+++ packages/natds-themes/build/react-native/index.js
@@ -1,36 +1,36 @@
 /* eslint-disable import/no-unresolved */
-import aesopDarkTheme from './aesop/dark.json';
-import aesopLightTheme from './aesop/light.json';
-import avonDarkTheme from './avon/dark.json';
-import avonLightTheme from './avon/light.json';
-import biomeDarkTheme from './biome/dark.json';
-import biomeLightTheme from './biome/light.json';
-import naturaDarkTheme from './natura/dark.json';
-import naturaLightTheme from './natura/light.json';
-import theBodyShopDarkTheme from './theBodyShop/dark.json';
-import theBodyShopLightTheme from './theBodyShop/light.json';
+import aesopDarkTheme from './aesop/dark.json'
+import aesopLightTheme from './aesop/light.json'
+import avonDarkTheme from './avon/dark.json'
+import avonLightTheme from './avon/light.json'
+import biomeDarkTheme from './biome/dark.json'
+import biomeLightTheme from './biome/light.json'
+import naturaDarkTheme from './natura/dark.json'
+import naturaLightTheme from './natura/light.json'
+import theBodyShopDarkTheme from './theBodyShop/dark.json'
+import theBodyShopLightTheme from './theBodyShop/light.json'
 
 const themes = {
   aesop: {
     dark: aesopDarkTheme,
-    light: aesopLightTheme,
+    light: aesopLightTheme
   },
   avon: {
     dark: avonDarkTheme,
-    light: avonLightTheme,
+    light: avonLightTheme
   },
   biome: {
     dark: biomeDarkTheme,
-    light: biomeLightTheme,
+    light: biomeLightTheme
   },
   natura: {
     dark: naturaDarkTheme,
-    light: naturaLightTheme,
+    light: naturaLightTheme
   },
   theBodyShop: {
     dark: theBodyShopDarkTheme,
-    light: theBodyShopLightTheme,
-  },
-};
+    light: theBodyShopLightTheme
+  }
+}
 
-export default themes;
+export default themes
