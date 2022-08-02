a2f1a296e761801f9f021c270e3185eec80fc29e
Lucas Lopes <dev.thuram@gmail.com>
2022-08-02 10:34:25 -0300

    test: remove all tests

--- packages/natds-themes/properties/platforms/html/elevation.test.js
+++
@@ -1,8 +0,0 @@
-import webElevations from '../web/elevation.json'
-import htmlElevations from './elevation'
-
-describe('Html Elevation', () => {
-  it('should return web elevations', () => {
-    expect(htmlElevations).toEqual(webElevations)
-  })
-})
--- packages/natds-themes/properties/platforms/html/typography.test.js
+++
@@ -1,8 +0,0 @@
-import webTypographies from '../web/typography.json'
-import cssTypography from './typography'
-
-describe('Css Typography', () => {
-  it('should return web typographies', () => {
-    expect(cssTypography).toEqual(webTypographies)
-  })
-})
