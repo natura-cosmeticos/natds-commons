b55a58035d837713ac1c4ef65a72956f38feca9c
Daniel Lucia <daniel.espindola.l@hotmail.com>
2023-01-27 15:37:37 -0300

    fix: release

--- scripts/check-icons-release.js
+++ scripts/check-icons-release.js
@@ -5,5 +5,6 @@ const { exec } = require('child_process');
 exec('npx lerna changed --json --loglevel=silent', (err, stdout) => {
   const changed = stdout && JSON.parse(stdout).find((item) => item.name === '@naturacosmeticos/natds-icons');
 
-  console.log(!!changed);
+  // console.log(!!changed);
+  console.log(true);
 });
