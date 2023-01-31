d873dd1d3da3c8908f6c5b73f343545f1d04ea5f
Daniel Lucia <daniel.espindola.l@hotmail.com>
2023-01-31 19:56:58 -0300

    fix(natds-theme): pipeline fix release

--- packages/natds-cli/bin/configureNpm.sh
+++ packages/natds-cli/bin/configureNpm.sh
@@ -4,3 +4,5 @@ set -e
 git fetch --tags
 npm config set '//registry.npmjs.org/:_authToken' '${NPM_TOKEN}'
 yarn config set _authToken '${NPM_TOKEN}'
+
+echo "//registry.yarnpkg.com/:_authToken=$NPM_TOKEN" >>~/.npmrc
