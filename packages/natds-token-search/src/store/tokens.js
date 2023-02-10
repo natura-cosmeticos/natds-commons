82f58537ba31d78982bc44046cd5ee2991551544
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2023-02-10 09:49:07 -0300

    feat(natds-token-search): add theme cb affects: @naturacosmeticos/natds-themes DSY-3041

--- packages/natds-token-search/src/store/tokens.js
+++ packages/natds-token-search/src/store/tokens.js
@@ -9,6 +9,12 @@ import webNaturaDark from '../../tokens/web/search/natura/dark'
 import webNaturaLight from '../../tokens/web/search/natura/light'
 import webTheBodyShopDark from '../../tokens/web/search/theBodyShop/dark'
 import webTheBodyShopLight from '../../tokens/web/search/theBodyShop/light'
+
+import webConsultoriaDeBelezaDark from '../../tokens/web/search/consultoriaDeBeleza/dark'
+import webConsultoriaDeBelezaLight from '../../tokens/web/search/consultoriaDeBeleza/light'
+import webNaturaV2Dark from '../../tokens/web/search/natura_v2/dark'
+import webNaturaV2Light from '../../tokens/web/search/natura_v2/light'
+
 import reactNativeAesopDark from '../../tokens/react-native/search/aesop/dark'
 import reactNativeAesopLight from '../../tokens/react-native/search/aesop/light'
 import reactNativeAvonDark from '../../tokens/react-native/search/avon/dark'
@@ -19,6 +25,12 @@ import reactNativeNaturaDark from '../../tokens/react-native/search/natura/dark'
 import reactNativeNaturaLight from '../../tokens/react-native/search/natura/light'
 import reactNativeTheBodyShopDark from '../../tokens/react-native/search/theBodyShop/dark'
 import reactNativeTheBodyShopLight from '../../tokens/react-native/search/theBodyShop/light'
+
+import reactNativeConsultoriaDeBelezaDark from '../../tokens/react-native/search/consultoriaDeBeleza/dark'
+import reactNativeConsultoriaDeBelezaLight from '../../tokens/react-native/search/consultoriaDeBeleza/light'
+import reactNativeNaturaV2Dark from '../../tokens/react-native/search/natura_v2/dark'
+import reactNativeNaturaV2Light from '../../tokens/react-native/search/natura_v2/light'
+
 import androidAesopDark from '../../tokens/android/search/aesop/dark'
 import androidAesopLight from '../../tokens/android/search/aesop/light'
 import androidAvonDark from '../../tokens/android/search/avon/dark'
@@ -29,6 +41,12 @@ import androidNaturaDark from '../../tokens/android/search/natura/dark'
 import androidNaturaLight from '../../tokens/android/search/natura/light'
 import androidTheBodyShopDark from '../../tokens/android/search/the_body_shop/dark'
 import androidTheBodyShopLight from '../../tokens/android/search/the_body_shop/light'
+
+import androidConsultoriaDeBelezaDark from '../../tokens/android/search/consultoria_de_beleza/dark'
+import androidConsultoriaDeBelezaLight from '../../tokens/android/search/consultoria_de_beleza/light'
+import androidNaturaV2Dark from '../../tokens/android/search/natura_v2/dark'
+import androidNaturaV2Light from '../../tokens/android/search/natura_v2/light'
+
 import iosAesopDark from '../../tokens/ios/search/aesop/dark'
 import iosAesopLight from '../../tokens/ios/search/aesop/light'
 import iosAvonDark from '../../tokens/ios/search/avon/dark'
@@ -39,6 +57,12 @@ import iosNaturaDark from '../../tokens/ios/search/natura/dark'
 import iosNaturaLight from '../../tokens/ios/search/natura/light'
 import iosTheBodyShopDark from '../../tokens/ios/search/theBodyShop/dark'
 import iosTheBodyShopLight from '../../tokens/ios/search/theBodyShop/light'
+
+import iosConsultoriaDeBelezaDark from '../../tokens/ios/search/consultoriaDeBeleza/dark'
+import iosConsultoriaDeBelezaLight from '../../tokens/ios/search/consultoriaDeBeleza/light'
+import iosNaturaV2Dark from '../../tokens/ios/search/natura_v2/dark'
+import iosNaturaV2Light from '../../tokens/ios/search/natura_v2/light'
+
 import { buildTokensForBrand } from '../helpers'
 
 export const tokens = {
@@ -47,28 +71,36 @@ export const tokens = {
     ...buildTokensForBrand('avon')([androidAvonLight, androidAvonDark]),
     ...buildTokensForBrand('biome')([androidBiomeLight, androidBiomeDark]),
     ...buildTokensForBrand('natura')([androidNaturaLight, androidNaturaDark]),
-    ...buildTokensForBrand('theBodyShop')([androidTheBodyShopLight, androidTheBodyShopDark])
+    ...buildTokensForBrand('theBodyShop')([androidTheBodyShopLight, androidTheBodyShopDark]),
+    ...buildTokensForBrand('consultoriaDeBeleza')([androidConsultoriaDeBelezaLight, androidConsultoriaDeBelezaDark]),
+    ...buildTokensForBrand('consultoriaDeBeleza')([androidNaturaV2Light, androidNaturaV2Dark])
   },
   ios: {
     ...buildTokensForBrand('aesop')([iosAesopLight, iosAesopDark]),
     ...buildTokensForBrand('avon')([iosAvonLight, iosAvonDark]),
     ...buildTokensForBrand('biome')([iosBiomeLight, iosBiomeDark]),
     ...buildTokensForBrand('natura')([iosNaturaLight, iosNaturaDark]),
-    ...buildTokensForBrand('theBodyShop')([iosTheBodyShopLight, iosTheBodyShopDark])
+    ...buildTokensForBrand('theBodyShop')([iosTheBodyShopLight, iosTheBodyShopDark]),
+    ...buildTokensForBrand('theBodyShop')([iosConsultoriaDeBelezaLight, iosConsultoriaDeBelezaDark]),
+    ...buildTokensForBrand('theBodyShop')([iosNaturaV2Light, iosNaturaV2Dark])
   },
   reactNative: {
     ...buildTokensForBrand('aesop')([reactNativeAesopLight, reactNativeAesopDark]),
     ...buildTokensForBrand('avon')([reactNativeAvonLight, reactNativeAvonDark]),
     ...buildTokensForBrand('biome')([reactNativeBiomeLight, reactNativeBiomeDark]),
     ...buildTokensForBrand('natura')([reactNativeNaturaLight, reactNativeNaturaDark]),
-    ...buildTokensForBrand('theBodyShop')([reactNativeTheBodyShopLight, reactNativeTheBodyShopDark])
+    ...buildTokensForBrand('theBodyShop')([reactNativeTheBodyShopLight, reactNativeTheBodyShopDark]),
+    ...buildTokensForBrand('consultoriaDeBeleza')([reactNativeConsultoriaDeBelezaLight, reactNativeConsultoriaDeBelezaDark]),
+    ...buildTokensForBrand('natura_v2')([reactNativeNaturaV2Light, reactNativeNaturaV2Dark])
   },
   web: {
     ...buildTokensForBrand('aesop')([webAesopLight, webAesopDark]),
     ...buildTokensForBrand('avon')([webAvonLight, webAvonDark]),
     ...buildTokensForBrand('biome')([webBiomeLight, webBiomeDark]),
     ...buildTokensForBrand('natura')([webNaturaLight, webNaturaDark]),
-    ...buildTokensForBrand('theBodyShop')([webTheBodyShopLight, webTheBodyShopDark])
+    ...buildTokensForBrand('theBodyShop')([webTheBodyShopLight, webTheBodyShopDark]),
+    ...buildTokensForBrand('consultoriaDeBeleza')([webConsultoriaDeBelezaLight, webConsultoriaDeBelezaDark]),
+    ...buildTokensForBrand('natura_v2')([webNaturaV2Light, webNaturaV2Dark])
   }
 }
 
