b2eace8d34d7650470333f9daff23831fbbf67b6
Design-System-Natura <65979129+Design-System-Natura@users.noreply.github.com>
2023-07-10 17:32:38 -0300

    Update build.js

--- packages/natds-themes/config/build.js
+++ packages/natds-themes/config/build.js
@@ -4,8 +4,6 @@ import { buildDictionary } from './shared/buildDictionary'
 export const build = () => brands
   .map((brand) => modes
     .map((mode) => platforms
-      .map((platform) => buildDictionary(brand, mode, platform))
-    )
-  )
+      .map((platform) => buildDictionary(brand, mode, platform))))
 
 export default build
