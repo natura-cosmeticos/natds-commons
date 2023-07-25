bb8e7d0143829dd6013cec3a6ed246df4da2205c
Design-System-Natura <65979129+Design-System-Natura@users.noreply.github.com>
2023-07-25 17:35:10 -0300

    js function to fix snakeCase on creating iOS files

--- packages/natds-themes/config/shared/helpers/textHelpers.js
+++ packages/natds-themes/config/shared/helpers/textHelpers.js
@@ -2,6 +2,8 @@ import { replace } from 'ramda'
 
 export const capitalizeWord = (word) => `${word[0].toUpperCase()}${word.slice(1)}`
 
+export const snakeToCamelCase = (str) => str.replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
+
 export const arrayToCamelCase = ([firstWord, ...words]) => `${firstWord}${words.map(capitalizeWord).join('')}`
 
 export const toSnakeCase = replace(/-/g, '_')
